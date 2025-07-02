0188ce44e181c76256e19ef782ae323ff4767740
- <>
2025-07-02 15:25:36 +0530

    -

---
+++ sg.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+tp=open('tp','w')
+import sys
+p=''
+for l in sys.stdin:
+    if l.startswith('<L>'):
+        l=l.split('<k2>')[1].split('<')[0].split(';')[0]
+        if l.find('/')!=-1 or l.find('^')!=-1:
+            l=l.replace('\u2014','').replace('-','')
+            if p!=l:
+                tp.write(l+'\n')
+            p=l
+
