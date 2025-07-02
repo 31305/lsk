c7bb75b563808b35d92e6fdba30a54e4be50234b
- <>
2025-07-02 15:48:53 +0530

    -

--- sg.py
+++ sg.py
@@ -6,7 +6,7 @@ for l in sys.stdin:
     if l.startswith('<L>'):
         l=l.split('<k2>')[1].split('<')[0].split(';')[0]
         if l.find('/')!=-1 or l.find('^')!=-1:
-            l=l.replace('\u2014','').replace('-','')
+            l=l.replace('\u2014','').replace('-','').replace('L','l')
             if p!=l:
                 tp.write(l+'\n')
             p=l
