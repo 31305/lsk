f2a91930aecc2729aa0975e001ae25b5dc617b8d
- <>
2025-11-15 11:38:40 +0530

    -

--- lsv.lua
+++ lsv.lua
@@ -1,4 +1,9 @@
 vim.keymap.set("n","<C-s>",function()
-	vim.fn.system("echo "..vim.fn.shellescape(vim.fn.getline("."))..
+	local l=vim.fn.getline(".")
+	local s=tonumber(l:match("^([^;]+);"))
+	if s then
+		vim.notify(os.date("%Y-%m-%d %H:%M:%S %Z",s))
+	end
+	vim.fn.system("echo "..vim.fn.shellescape(l)..
 	"|sed \"s/^[0-9]*\\;//g\"|sed \"s/\\;$//g\"|sed \"s/\\;/\\n/g\"|sed \"s/\\,/ /g\"|sksl|sox -t au - -d")
 end)
