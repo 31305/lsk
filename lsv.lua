ad14aa9de54d94c55b84214ac4c154af1577c250
- <>
2025-11-15 11:13:41 +0530

    -

--- lsv.lua
+++ lsv.lua
@@ -1,4 +1,4 @@
 vim.keymap.set("n","<C-s>",function()
 	vim.fn.system("echo "..vim.fn.shellescape(vim.fn.getline("."))..
-	"|sed \"s/^\\;//g\"|sed \"s/\\;$//g\"|sed \"s/\\;/\\n/g\"|sed \"s/\\,/ /g\"|sksl|sox -t au - -d")
+	"|sed \"s/^[0-9]*\\;//g\"|sed \"s/\\;$//g\"|sed \"s/\\;/\\n/g\"|sed \"s/\\,/ /g\"|sksl|sox -t au - -d")
 end)
