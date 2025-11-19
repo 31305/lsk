fcbdebb863b8845bea17f5ef6430d6d68fac6264
- <>
2025-11-19 15:16:34 +0530

    -

--- lsv.lua
+++ lsv.lua
@@ -1,8 +1,19 @@
 vim.keymap.set("n","<C-s>",function()
 	local l=vim.fn.getline(".")
-	local s=tonumber(l:match("^([^;]+);"))
-	if s and s>=1734424182 then
-		vim.notify(os.date("%Y-%m-%d %H:%M:%S %Z",s))
+	if vim.fn.getline(1)==";1,55,51,1,45,37,51,2,77;" then
+		local s1,s2=unpack(vim.api.nvim_win_get_cursor(0))
+		local ps=l:sub(1,s2):find(".*;")
+		local ds=l:sub(s2+2):find(";")
+		if ps and ds then
+			l=l:sub(ps,s2+2+ds)
+			vim.notify(l)
+		else return
+		end
+	else
+		local s=tonumber(l:match("^([^;]+);"))
+		if s and s>=1734424182 then
+			vim.notify(os.date("%Y-%m-%d %H:%M:%S %Z",s))
+		end
 	end
 	vim.fn.system("echo "..vim.fn.shellescape(l)..
 	"|sed \"s/^[0-9\\/#]*\\;//g\"|sed \"s/\\;$//g\"|sed \"s/\\;/\\n/g\"|sed \"s/\\,/ /g\"|sksl|sox -t au - -d")
