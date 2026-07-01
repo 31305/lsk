ab8ceacb6a8298aabb35a3b9251d62a0b04065b3
- <>
2026-07-01 10:11:46 +0530

    -

--- lsv.lua
+++ lsv.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n","<C-s>",function()
+function sv()
 	local l=vim.fn.getline(".")
 	if vim.fn.getline(1)==";1,55,51,1,45,37,51,2,77;" then
 		local s1,s2=unpack(vim.api.nvim_win_get_cursor(0))
@@ -16,4 +16,11 @@ vim.keymap.set("n","<C-s>",function()
 	end
 	vim.fn.system("echo "..vim.fn.shellescape(l)..
 	"|sed \"s/^[0-9\\/#]*\\;//g\"|sed \"s/\\;$//g\"|sed \"s/\\;/\\n/g\"|sed \"s/\\,/ /g\"|sksl|sox -t au - -d")
+end
+vim.keymap.set("n","_",function()
+	sv()
+end)
+vim.keymap.set("n","-",function()
+	vim.cmd("normal! j")
+	sv()
 end)
